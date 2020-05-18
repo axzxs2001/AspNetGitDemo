3511a9d0fecaf890a9140b4df603cf5a83902136
桂素伟 <axzxs2001@163.com>
2020-05-18 14:21:28 +0900

    修改所有运算中a为nunber1,b为number2。

--- AspNetGitDemo/Web001/Services/Calculator.cs
+++ AspNetGitDemo/Web001/Services/Calculator.cs
@@ -10,34 +10,34 @@ namespace Web001.Services
         /// <summary>
         /// 相加
         /// </summary>
-        /// <param name="a">a</param>
-        /// <param name="b">b</param>
-        /// <returns>a+b的和</returns>
-        public double Plus(double a, double b)
+        /// <param name="number1">number1</param>
+        /// <param name="number2">number2</param>
+        /// <returns>number1+number2的和</returns>
+        public double Plus(double number1, double number2)
         {
-            return a + b;
+            return number1 + number2;
         }
 
         /// <summary>
         /// 相减
         /// </summary>
-        /// <param name="a">a</param>
-        /// <param name="b">b</param>
-        /// <returns>a-b的差</returns>
-        public double Subtraction(double a, double b)
+        /// <param name="number1">number1</param>
+        /// <param name="number2">number2</param>
+        /// <returns>number1-number2的差</returns>
+        public double Subtraction(double number1, double number2)
         {
-            return a - b;
+            return number1 - number2;
         }
 
         /// <summary>
         /// 相除
         /// </summary>
-        /// <param name="a">a</param>
-        /// <param name="b">b</param>
-        /// <returns>a/b的商</returns>
-        public double Division(double a, double b)
+        /// <param name="number1">number1</param>
+        /// <param name="number2">number2</param>
+        /// <returns>number1/number2的商</returns>
+        public double Division(double number1, double number2)
         {
-            return a / b;
+            return number1 / number2;
         }
     }
 }
